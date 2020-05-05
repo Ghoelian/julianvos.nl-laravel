3ed380effdefb2a59d83039494518fb41632fc7c
Julian Vos <post@julianvos.nl>
2020-05-05 15:52:06 +0200

    Fix W3.css

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <title>Julian Vos</title>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="styles/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
   integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
