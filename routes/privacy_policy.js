2d5507ef913048af976ec631fdaccf46be52dae6
Julian Vos <post@julianvos.nl>
2020-07-19 20:58:26 +0200

    Add terms of service and privacy policy

--- routes/privacy_policy.js
+++ routes/privacy_policy.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('projects', { title: 'Projects' })
+  res.render('privacy_policy', { title: 'Privacy Policy' })
 })
 
 module.exports = router
--- routes/terms_of_service.js
+++ routes/terms_of_service.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('projects', { title: 'Projects' })
+  res.render('terms_of_service', { title: 'Terms of Service' })
 })
 
 module.exports = router
