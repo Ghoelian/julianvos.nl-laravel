38e4baa3363fa6e2277d102ed7752997e3c2e843
Julian Vos <post@julianvos.nl>
2021-03-06 12:03:03 +0100

    Add social media links to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,6 +4,14 @@
             <span class="material-icons md-18">copyright</span> Copyright 2021 Julian Vos
         </div>
         <div>
+            <p style="margin-bottom: 0px;">
+                <span style="font-size: 2em;">
+                    <a href="https://github.com/Ghoelian" class="footer-link"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/julian-vos-b09783150/" class="footer-link"><i class="fab fa-linkedin"></i></a>
+                </span>
+            </p>
+        </div>
+        <div>
             <a class="footer-link" href="{{ route('privacy-policy') }}">Privacy Policy</a>
             |
             <a class="footer-link" href="{{ route('terms-of-service') }}">Terms of Service</a>
