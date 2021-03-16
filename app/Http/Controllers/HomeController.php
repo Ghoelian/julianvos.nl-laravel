68d31f159f96f0b06aa1d74d618509d75718ac72
Julian Vos <post@julianvos.nl>
2021-03-16 21:52:26 +0100

    Add verify captcha route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,29 @@ class HomeController extends Controller
     public function termsOfService()
     {
         return view('termsOfService');
+    }
+
+    public function verifyRecaptcha(Request $request) {
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = ['secret' => env('RECAPTCHA_SECRET'), 'response' => $request->input('recaptcha_token')];
+
+        $options = [
+            'http' => [
+                'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $result = json_decode(file_get_contents($url, false, $context));
+
+        if ($result->success && $result->score >= 0.5) {
+            return '<p><span class="material-icons md-18">location_on</span> Klazienaveen, Drenthe, NL</p>
+            <p><span class="material-icons md-18">call</span> +31 6 48744406</p>
+            <p><span class="material-icons md-18">email</span> <a href="mailto://post@julianvos.nl" />post@julianvos.nl</p>';
+        } else {
+            return false;
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@getHome')->name('home');
 
+Route::post('recaptcha', 'App\Http\Controllers\HomeController@verifyRecaptcha')->name('recaptcha');
+
 Route::get('privacy-policy', 'App\Http\Controllers\HomeController@privacyPolicy')->name('privacy-policy');
 Route::get('terms-of-service', 'App\Http\Controllers\HomeController@termsOfService')->name('terms-of-service');
